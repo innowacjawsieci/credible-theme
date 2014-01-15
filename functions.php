5a430c51cd5670524f5416cfe6a7071fb3eee516
innowacjawsieci <kontakt@innowacjawsieci.pl>
2014-01-15 21:16:21 +0100

    Revert "file wordpress theme"
    This reverts commit 2e749f7466239140b2e0868378695c1662260f38.

--- functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//Initialize the update checker.
-require 'theme-updates/theme-update-checker.php';
-$example_update_checker = new ThemeUpdateChecker(
-	'credible-theme',                                            //Theme folder name, AKA "slug". 
-	'http://salesapp.pl/files/credible-theme-updates/info.json' //URL of the metadata file.
-);
-
